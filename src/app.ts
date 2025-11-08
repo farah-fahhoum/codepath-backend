1b17b973d722e5b3aa9fbeee1ef947f7def0dc45
Farah <farah.fahhoum3@gmail.com>
2025-11-08 14:49:48 +0300

    Add auth, user and contact routes to express app

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,10 @@ import express from "express";
 import morgan from "morgan";
 import fs from "fs";
 
+import { router as authRoutes } from "./routes/auth.api";
+import { router as userRoutes } from "./routes/user.api";
+import { router as contactRoutes } from "./routes/contact.api";
+
 const app = express();
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
@@ -14,6 +18,10 @@ app.use(
 );
 
 app.use(cors({ origin: ["http://localhost:3000"] }));
+
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/contact", contactRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
