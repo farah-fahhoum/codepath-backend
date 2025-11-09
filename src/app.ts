7cd0cb985735eedec3a95ea21b0b61bb1ae1b3b5
Farah <farah.fahhoum3@gmail.com>
2025-11-09 21:42:58 +0300

    Add quiz routes to express app

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import fs from "fs";
 import { router as authRoutes } from "./routes/auth.api";
 import { router as userRoutes } from "./routes/user.api";
 import { router as contactRoutes } from "./routes/contact.api";
+import { router as quizRoutes } from "./routes/quiz.api";
 
 const app = express();
 app.use(express.json({ limit: "20mb" }));
@@ -22,6 +23,7 @@ app.use(cors({ origin: ["http://localhost:3000"] }));
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/contact", contactRoutes);
+app.use("/quiz", quizRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
